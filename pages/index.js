8cdebb58e930c5ddf1e72ecb77be9b28732d62cb
aufung1999 <78595669+aufung1999@users.noreply.github.com>
2023-04-30 04:30:09 -0400

    List Products

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,19 @@ import Image from "next/image";
 import { Inter } from "@next/font/google";
 
 import Layout from "@/components/Layout";
+import data from "@/utils/data";
+import ProductItems from "@/components/ProductItems";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <Layout title = "Hi">HOME Page</Layout>;
+  return (
+    <Layout title="Hi">
+      <div className=" grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products?.map((each) => (
+          <ProductItems product={each} key={each.slug} />
+        ))}
+      </div>
+    </Layout>
+  );
 }
